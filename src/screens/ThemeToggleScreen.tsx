22d3ef7fd370f1008e8c4e1c80dd7c7c5295bbc9
Damoiskii <moimyazz@gmail.com>
2026-09-16 11:52:45 -0500

    feat: add ThemeProvider component and enhance ThemeToggleScreen with theme context

--- src/screens/ThemeToggleScreen.tsx
+++ src/screens/ThemeToggleScreen.tsx
@@ -1,4 +1,11 @@
-import { Appearance, Pressable, Text, useColorScheme, View } from "react-native";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import {
+    Appearance,
+    Pressable,
+    Text,
+    useColorScheme,
+    View,
+} from "react-native";
 
 const ThemeToggleScreen = () => {
   const colorScheme = useColorScheme();
@@ -10,22 +17,28 @@ const ThemeToggleScreen = () => {
   };
 
   return (
-    <View className='flex-1 w-full items-center justify-center dark:bg-gray-900'>
-      <Text className='text-lg font-bold mb-4 dark:text-white'>
-        Theme Toggle Screen
-      </Text>
+    <ThemeProvider>
+      <View className='flex-1 w-full items-center justify-center bg-background'>
+        <View className='absolute top-0 right-[-100] w-80 h-80 bg-purple-200 dark:bg-cyan-200 rounded-full opacity-20' />
 
-      <Pressable
-        onPress={toggleTheme}
-        className='p-4 bg-gray-200 dark:bg-gray-800 rounded-lg'
-      >
-        <Text className='text-black dark:text-white'>Toggle Theme</Text>
-      </Pressable>
+        <Text className='text-lg font-bold mb-4 dark:text-white'>
+          Theme Toggle Screen
+        </Text>
 
-      <Text className='mt-4 dark:text-white'>
-        Current Theme: {colorScheme || "unspecified"}
-      </Text>
-    </View>
+        <Pressable
+          onPress={toggleTheme}
+          className='p-4 bg-gray-200 dark:bg-gray-800 rounded-lg'
+        >
+          <Text className='text-black dark:text-white'>Toggle Theme</Text>
+        </Pressable>
+
+        <Text className='mt-4 dark:text-white'>
+          Current Theme: {colorScheme || "unspecified"}
+        </Text>
+
+        <View className='absolute bottom-0 left-[-100] w-50 h-50 bg-cyan-200 dark:bg-purple-200 rounded-full opacity-20' />
+      </View>
+    </ThemeProvider>
   );
 };
 
